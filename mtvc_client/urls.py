9996c00918ea98fabbc28dd7bf626977fede0d4f
Max Naude <maxnaude@gmail.com>
2014-01-10 22:28:02 +0200

    Fixed view name

--- mtvc_client/urls.py
+++ mtvc_client/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'^channel/$', views.ChannelsView.as_view(), name='channels'),
     url(r'^channel/(?P<slug>[\w-]+)/$', views.EPGView.as_view(), name='epg'),
     url(r'^show/$', views.ShowsView.as_view(), name='shows'),
-    url(r'^clip/$', views.ShowsView.as_view(), name='clips'),
+    url(r'^clip/$', views.ClipsView.as_view(), name='clips'),
     url(r'^(?P<content_type>channel|clip)/(?P<slug>[\w-]+)/watch/$',
         views.WatchView.as_view(), name='watch'),
     url(r'^help/$', views.HelpView.as_view(), name='help'),
