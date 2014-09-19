4046316c568b345ac3ae1503ba8769b5d4881aea
Max Naude <maxnaude@gmail.com>
2014-09-19 16:11:08 +0200

    Added README, LICENSE and classifiers for setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ setup(
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
     url='https://github.com/praekelt/mtvc-api-client',
-    packages = find_packages(),
-    dependency_links = [],
-    install_requires = [
+    packages=find_packages(),
+    dependency_links=[],
+    install_requires=[
         'hammock==0.2.4'
     ],
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
 )
