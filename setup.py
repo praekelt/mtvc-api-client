c128075d0bf8d3d140dc390ed5fdba4483268f0c
Max Naude <maxnaude@gmail.com>
2015-07-14 15:43:16 +0200

    Bumped version to 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mtvc-api-client',
-    version='0.0.8',
+    version='0.0.9',
     description='Praekelt MTVC API Client',
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
